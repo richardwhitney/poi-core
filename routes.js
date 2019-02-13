73ccc92f1fdbdc99f435fc36a1aa57b121a70e1e
Richard Whitney <rwhitney32@gmail.com>
2019-02-13 23:15:16 +0000

    added support for app to render images

--- routes.js
+++ routes.js
@@ -1,3 +1,13 @@
 const PointOfInterest = require('./app/controllers/pointOfInterest');
 
-module.exports = [{ method: 'GET', path: '/', config: PointOfInterest.home}];
+module.exports = [
+  { method: 'GET', path: '/', config: PointOfInterest.home},
+  { method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './public'
+      }
+    }
+  }
+];
