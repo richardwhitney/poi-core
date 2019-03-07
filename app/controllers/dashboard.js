5948a55f7a38d001a856ffd80d19357fab5c8e22
Richard Whitney <rwhitney32@gmail.com>
2019-03-07 23:05:34 +0000

    user can now select category when adding points

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,21 +1,25 @@
 'use strict';
 
 const PointOfInterest = require('../models/poi');
+const Category = require('../models/category');
 const Joi = require('joi');
 
 const Dashboard = {
   home: {
     handler: async function (request, h) {
       const pointsofInterest = await PointOfInterest.find().populate('category');
+      const categories = await Category.find();
       return h.view('dashboard', {
         title: 'Explore Islands of Ireland',
-        points: pointsofInterest
+        points: pointsofInterest,
+        categories: categories
       });
     }
   },
   addPointOfInterest: {
     validate: {
       payload: {
+        category: Joi.string().required(),
         name: Joi.string().required(),
         description: Joi.string().required()
       },
@@ -24,9 +28,11 @@ const Dashboard = {
       },
       failAction: async function (request, h, error) {
         const pointsOfInterest = await PointOfInterest.find();
+        const categories = await Category.find();
         return h.view('dashboard', {
           title: 'Add island error',
           points: pointsOfInterest,
+          categories: categories,
           errors: error.details
         }).takeover().code(400);
       }
@@ -34,9 +40,14 @@ const Dashboard = {
     handler: async function(request, h) {
       try {
         const data = request.payload;
+        const rawCategory = request.payload.category;
+        const category = await Category.findOne({
+          name: rawCategory
+        });
         const newPoint = new PointOfInterest({
           name: data.name,
-          description: data.description
+          description: data.description,
+          category: category._id
         });
         await newPoint.save();
         return h.redirect('/home');
